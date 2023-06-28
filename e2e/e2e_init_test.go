afacc87ca253d502ba6aca3017b46d082068813b
Pavol Ipoth <pavol.ipoth@protonmail.com>
2023-06-28 16:39:39 +0200

    Use ginkgo in E2E tests

---
+++ e2e/e2e_init_test.go
@@ -0,0 +1,16 @@
+//go:build e2e
+// +build e2e
+
+package e2e_test
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+)
+
+var _ = BeforeSuite(func(ctx context.Context) {
+})
+
+var _ = AfterSuite(func() {
+})
