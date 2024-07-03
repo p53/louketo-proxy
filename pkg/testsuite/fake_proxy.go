ef3f778396317e9f2dd148fccc73ff28ac51b379
Pavol Ipoth <pavol.ipoth@protonmail.com>
2024-07-04 01:00:40 +0200

    Fix linting warnings

--- pkg/testsuite/fake_proxy.go
+++ pkg/testsuite/fake_proxy.go
@@ -735,7 +735,7 @@ func makeTestCodeFlowLogin(location string, xforwarded bool) (*http.Response, []
 	}
 	// step: get the redirect
 	var resp *http.Response
-	for count := 0; count <= 4; count++ {
+	for range 4 {
 		req, err := http.NewRequest(http.MethodGet, location, nil)
 
 		for _, cookie := range flowCookies {
